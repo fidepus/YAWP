e1898726c2d383cca2f257be9b127b5d6992fea6
fidepus <mail@fidepus.de>
2016-12-22 00:44:24 +0100

    Improved LCD-handling.

--- backlight.py
+++ backlight.py
@@ -8,18 +8,18 @@ import lcddriver
 lcd = lcddriver.lcd()
 
 def switch_light():
-	# get the time
-	now = datetime.now()
-	now_time = now.time()
+    # get the time
+    now = datetime.now()
+    now_time = now.time()
 
-	# see if it is time to switch on the light
-	if now_time >= time(6,00) and now_time <= time(23,00):
-	    lcd.backlight(1)
-	    lcd2.lcd_clear()
-		lcd2.lcd_display_string(time.strftime("%d.%m.%Y %H:%M"), 1)
-		lcd2.lcd_display_string('Innen: {0} Grad'.format(Temperature_In), 2)
-		lcd2.lcd_display_string('Aussen: {0} Grad'.format(Temperature_Out), 3)
-		lcd2.lcd_display_string(str(Weather_Text) + ' ' + str(humidityr) + '%', 4)
-        # if not, switch it off
-	else:
-		lcd.backlight(0)
+    # see if it is time to switch on the light
+    if now_time >= time(6,00) and now_time <= time(23,00):
+        lcd.backlight(1)
+        lcd.lcd_clear()
+        lcd.lcd_display_string(time.strftime("%d.%m.%Y %H:%M"), 1)
+        lcd.lcd_display_string('Innen: {0} Grad'.format(Temperature_In), 2)
+        lcd.lcd_display_string('Aussen: {0} Grad'.format(Temperature_Out), 3)
+        lcd.lcd_display_string(str(Weather_Text) + ' ' + str(humidityr) + '%', 4)
+    # if not, switch it off
+    else:
+        lcd.backlight(0)
